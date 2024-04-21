e8e28ca1e3ee82ac283c93becab6293f39c53c98
nulluser0 <diuyueng@gmail.com>
2024-04-21 14:49:14 +1000

    updated to make the kernel a higher half kernel (i swear i didnt just yoink code from someone else lmao)

--- kernel/tiramisu_bootloader/src/bios/boot.asm
+++ kernel/tiramisu_bootloader/src/bios/boot.asm
@@ -1,6 +1,6 @@
-section .text
-bits 32
 global _start
+section .init.text32
+bits 32
 _start:
     ; We are now in i386 (32-bit) protected mode.
     ; Interrupts are disabled.
@@ -8,7 +8,7 @@ _start:
     ; Kernel has full control over the CPU.
 
     ; Set up the stack
-    mov esp, stack_top
+    mov esp, stack_top - KERNEL_OFFSET
 
     ; Make pointer accessible to rust entry point. Do not change edi before calling entry.
     mov edi, ebx
@@ -23,12 +23,10 @@ _start:
     call enable_paging          ; Enables paging.
 
     ; load the 64-bit GDT
-    lgdt [gdt64.pointer]
+    lgdt [gdt64.pointer_low - KERNEL_OFFSET]
+
+    ; Jump to the address of long_mode_start
     jmp gdt64.code:long_mode_start
-    
-    ; Inaccessible
-    mov al, 'F'
-	jmp _error_occurred
 
 check_multiboot2:
 	cmp eax, 0x36d76289 ; Check eax if magic exists. GRUB will put magic on eax.
@@ -95,78 +93,130 @@ global _error_occurred
 _error_occurred:
 	mov dword [0xb8000], 0x4f524f45
 	mov dword [0xb8004], 0x4f3a4f52
-	mov dword [0xb8008], 0x4f204f20  ; Print "ERROR" at 0xb8000 ; Prints ERROR at 0xb8000
+	mov dword [0xb8008], 0x4f204f20  ; Print "ERR" at 0xb8000 ; Prints ERROR at 0xb8000
 	mov byte  [0xb800a], al ; Display error code (at 'al') as ASCII character
 	hlt
 
 set_up_page_tables:
-    ; map first P4 entry to P3 table
-    mov eax, p3_table
-    or eax, 0b11 ; present + writable
-    mov [p4_table], eax
+	; Set up recursive paging at the second to last entry
+	mov eax, p4_table - KERNEL_OFFSET
+	or eax, 11b ; present + writable
+	mov [(p4_table - KERNEL_OFFSET) + (510 * 8)], eax
 
-    ; map first P3 entry to P2 table
-    mov eax, p2_table
-    or eax, 0b11 ; present + writable
-    mov [p3_table], eax
+	; map the first P4 entry to the first P3 table
+	;
+	; This will be changed to the page containing
+	; only the first megabyte before rust starts
+	mov eax, low_p3_table - KERNEL_OFFSET
+	or eax, 11b ; present + writable
+	mov [p4_table - KERNEL_OFFSET], eax
 
-    mov ecx, 0         ; counter variable
-.map_p2_table:
-    ; map ecx-th P2 entry to a huge page that starts at address 2MiB*ecx
-    mov eax, 0x200000  ; 2MiB
-    mul ecx            ; start address of ecx-th page
-    or eax, 0b10000011 ; present + writable + huge
-    mov [p2_table + ecx * 8], eax ; map ecx-th entry
+	; map the last P4 entry to last P3 table
+	mov eax, high_p3_table - KERNEL_OFFSET
+	or eax, 11b ; present + writable
+	mov [p4_table - KERNEL_OFFSET + (511 * 8)], eax
 
-    inc ecx            ; increase counter
-    cmp ecx, 512       ; if counter == 512, the whole P2 table is mapped
-    jne .map_p2_table  ; else map the next entry
+	; map first entry of the low P3 table to the kernel table
+	mov eax, kernel_table - KERNEL_OFFSET
+	or eax, 11b ; present + writable
+	mov [low_p3_table - KERNEL_OFFSET], eax
+	; now to the second to highest entry of the high P3 table
+	mov [high_p3_table - KERNEL_OFFSET + (510 * 8)], eax
 
-    ret
+	; map each P2 entry to a huge 2MiB page
+	mov ecx, 0x0       ; counter variable
+
+.map_kernel_table:
+	mov eax, 0x200000  ; 2MiB
+	mul ecx            ; eax now holds the start address of the ecx-th page
+	or eax, 10000011b  ; present + writable + huge
+	mov [(kernel_table - KERNEL_OFFSET) + (ecx * 8)], eax ; map ecx-th entry
+
+	inc ecx            ; increase counter
+	cmp ecx, 512       ; if counter == 512, the whole P2 table is mapped
+	jne .map_kernel_table  ; else map the next entry
+
+	; map the first P2 entry to the megabyte table
+	mov eax, megabyte_table - KERNEL_OFFSET
+	or eax, 11b
+	mov [low_p2_table - KERNEL_OFFSET], eax
+
+	; identity map the first megabyte
+	mov ecx, 0x0
+
+.map_megabyte_table:
+	mov eax, 4096      ; 4Kb
+	mul ecx            ; start address of ecx-th page
+	or eax, 11b        ; present + writable
+	mov [(megabyte_table - KERNEL_OFFSET) + (ecx * 8)], eax ; map ecx-th entry
+
+	inc ecx            ; increase counter
+	cmp ecx, 256       ; if counter = 256, the whole megabyte is mapped
+	jne .map_megabyte_table ; else map the next entry
+
+	ret
+
+
+unmap_guard_page:
+	; put the address of the stack guard huge pages into ecx
+	mov ecx, (bsp_stack_guard_page - 0x200000 - KERNEL_OFFSET)
+	shr ecx, 18      ; calculate P2 index
+	and ecx, 0x1FF  ; get P2 index by itself
+	; ecx now holds the index into the P2 page table of the entry we want to unmap
+	mov eax, 0x0  ; set huge page flag, clear all others
+	mov [(kernel_table - KERNEL_OFFSET) + ecx], eax ; unmap (clear) ecx-th entry
+	ret
+
 
 enable_paging:
-    ; load P4 to cr3 register (cpu uses this to access the P4 table)
-    mov eax, p4_table
-    mov cr3, eax
+	; Enable:
+	;     PGE: (Page Global Extentions)
+	;     PAE: (Physical Address Extension)
+	;     PSE: (Physical Size Extentions)
+	mov eax, cr4
+	or eax, (1 << 7) | (1 << 5) | (1 << 1)
+	mov cr4, eax
 
-    ; enable PAE-flag in cr4 (Physical Address Extension)
-    mov eax, cr4
-    or eax, 1 << 5
-    mov cr4, eax
+	; load P4 to cr3 register (cpu uses this to access the P4 table)
+	mov eax, p4_table - KERNEL_OFFSET
+	mov cr3, eax
 
-    ; set the long mode bit in the EFER MSR (model specific register)
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 1 << 8
-    wrmsr
+	; set the no execute (bit 11), long mode (bit 8), and SYSCALL Enable (bit 0)
+	; bits in the EFER MSR (model specific register)
+	mov ecx, 0xC0000080
+	rdmsr
+	or eax, (1 <<11) | (1 << 8) | (1 << 0) ; NXE, LME, SCE
+	wrmsr
 
-    ; enable paging in the cr0 register
-    mov eax, cr0
-    or eax, 1 << 31
-    mov cr0, eax
+	; enable paging and write protection in the cr0 register
+	mov eax, cr0
+	or eax, (1 << 31) | (1 << 16) ; PG | WP
+	mov cr0, eax
 
-    ret
-
-section .rodata
-gdt64:
-    dq 0 ; zero entry
-.code: equ $ - gdt64 ; new
-    dq (1<<43) | (1<<44) | (1<<47) | (1<<53) ; code segment
-.pointer:
-    dw $ - gdt64 - 1
-    dq gdt64
+	ret
 
 ;
 ; --- LONG MODE ---
 ;
 
 global long_mode_start
+section .init.text.high
+bits 64
+long_mode_start:
+    lgdt [rel gdt64.pointer]
+    mov rax, start_high
+	jmp rax
+
+global start_high
 extern rust_entry
 section .text
 bits 64
-long_mode_start:
-    ; load 0 into all data segment registers
-    mov ax, 0
+start_high:
+
+    add rsp, KERNEL_OFFSET
+
+    ; load gdt64.data into all data segment registers
+    mov ax, gdt64.data
     mov ss, ax
     mov ds, ax
     mov es, ax
@@ -178,14 +228,57 @@ long_mode_start:
     call rust_entry
     hlt
 
-section .bss
+section .page_table nobits alloc noexec write
 align 4096
 p4_table:
     resb 4096
-p3_table:
-    resb 4096
-p2_table:
-    resb 4096
+
+section .bss
+low_p3_table:
+	resb 4096
+high_p3_table:
+	resb 4096
+low_p2_table:
+	resb 4096
+megabyte_table:
+	resb 4096
+kernel_table:
+	resb 4096
+
+; 2MB space here
+section .guard_huge_page nobits noalloc noexec nowrite
+
+
+; We use page alignment (4096B) for convenience and compatibility 
+; with the stack abstractions in Rust. 
+; We place the stack in its own sections for loading/parsing convenience.
+; Note: The `bsp_stack_guard_page` is mapped by the boot-time page tables. 
+section .stack nobits alloc noexec write
+align 4096 
+global bsp_stack_guard_page
+bsp_stack_guard_page:
+	resb 4096
+global bsp_stack_bottom
 stack_bottom:
-    resb 64
+	resb 4096 * 32
+global bsp_stack_top
 stack_top:
+	resb 4096
+double_fault_stack_top:
+
+KERNEL_OFFSET equ 0xFFFFFFFF80000000
+
+section .rodata
+gdt64:
+    dq 0 ; zero entry
+.code: equ $ - gdt64
+    dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; code segment
+.data: equ $ - gdt64
+    dq (1<<44) | (1<<47) | (1<<41) ; data segment
+.end equ $
+.pointer_low:
+    dw .end - gdt64 - 1
+    dd gdt64 - KERNEL_OFFSET
+.pointer:
+    dw .end - gdt64 - 1
+    dq gdt64
