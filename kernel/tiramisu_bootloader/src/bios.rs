f7bb1ed54d86ff92401642fba9a26733c150d77a
nulluser0 <diuyueng@gmail.com>
2024-06-04 19:16:45 +1000

    rust fmt

--- kernel/tiramisu_bootloader/src/bios.rs
+++ kernel/tiramisu_bootloader/src/bios.rs
@@ -7,18 +7,18 @@ pub extern "C" fn rust_entry() -> ! {
 
     let mut hello_colored = [color_byte; 24];
     for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
+        hello_colored[i * 2] = *char_byte;
     }
 
     // write `Hello World!` to the center of the VGA text buffer
     let buffer_ptr = (0xb8000 + 1988) as *mut _;
     unsafe { *buffer_ptr = hello_colored };
 
-    loop{}
+    loop {}
 }
 
 #[no_mangle]
-pub extern fn eh_personality() {}
+pub extern "C" fn eh_personality() {}
 
 #[panic_handler]
 fn panic_fmt(_info: &core::panic::PanicInfo) -> ! {
